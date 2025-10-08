5d18b44331a3ea7e401f0bcc0123e65937a922bf
aravind-3697 <43001827+aravind-3697@users.noreply.github.com>
2025-10-08 22:22:40 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,9 @@
+from payment_processor import PaymentMode, checkout
+
+if __name__ == "__main__":
+    amount = 150.75
+
+    checkout(PaymentMode.PAYPAL, amount)
+    checkout(PaymentMode.GOOGLEPAY, amount)
+    checkout(PaymentMode.CREDITCARD, amount)
+    checkout(PaymentMode.UNKNOWN, amount)
