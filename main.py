faf7eb046a7685457d006732c6688875930d7b54
aravind-3697 <43001827+aravind-3697@users.noreply.github.com>
2025-10-09 09:28:16 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,9 @@
-from payment_processor.factory import PaymentFactory
-
-def checkout(payment_type: str, amount: float):
-    if amount <= 0:
-        print("❌ Invalid payment amount.")
-        return
-
-    processor = PaymentFactory.get_processor(payment_type)
-    if processor:
-        processor.process_payment(amount)
-    else:
-        print(f"❌ Unsupported payment mode: '{payment_type}'")
-
+from payment_processor import PaymentMode, checkout
 
 if __name__ == "__main__":
-    checkout("paypal", 150.00)
-    checkout("googlepay", 200.00)
-    checkout("creditcard", 300.00)
-    checkout("applepay", 400.00)  # unsupported example
+    amount = 150.75
+
+    checkout(PaymentMode.PAYPAL, amount)
+    checkout(PaymentMode.GOOGLEPAY, amount)
+    checkout(PaymentMode.CREDITCARD, amount)
+    checkout(PaymentMode.UNKNOWN, amount)
